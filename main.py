02756286b8d8e6bfaa548f23634f52253f473ee0
Yoseff Abu Dayeh <yoseffsalim@gmail.com>
2026-09-23 15:46:44 -0400

    API with bare setup of fastapi + uvicorn works.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = FastAPI()
 
 app.include_router(generalController.router, tags=["general"])
 
+
+#To get the API UI then one has to do /docs
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
