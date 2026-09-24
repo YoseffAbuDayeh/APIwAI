cf90d94c9f75245de50d32aa67756fd281556ded
Yoseff Abu Dayeh <yoseffsalim@gmail.com>
2026-09-24 15:27:30 -0400

    Added comments to the imageHelper.py explaining most parts of the code

--- methods/imageHelper.py
+++ methods/imageHelper.py
@@ -14,40 +14,50 @@ def segmentLines(img: PILImage.Image, min_gap=8):
     gray = np.array(img.convert("L")) #Makes the image grayscale for easier understanding
 
     # We get the high and low brightness points to distinguish the Ink and the Paper.
-    # It's called the Otsu's method according to my research
+    # It is approximate to Otsu's method which gets an auto-threshold.
     flat = gray.flatten()
     low, high = np.percentile(flat, [1, 95])
     binarize_threshold = (low + high) / 2
 
-
+    # We compare each number in gray to see if it is ink or if it is blank
     dark_pixels = gray < binarize_threshold
+
+    #At this point darkPixels are an array of true and false. This sums them up to see how many dark pixels the row has
     dark_count_per_row = dark_pixels.sum(axis=1)
-    print(f"Dark pixel counts per row - min: {dark_count_per_row.min()}, max: {dark_count_per_row.max()}, mean: {dark_count_per_row.mean()}")
 
+    #Gets 1% of the amount of pixels the row can have.
     min_dark_pixels = gray.shape[1] * 0.01
-    print(f"min_dark_pixels threshold: {min_dark_pixels}")
 
+    #Checks if the row could have text.
     is_text_row = dark_count_per_row > min_dark_pixels
-    pattern = "".join("T" if t else "." for t in is_text_row)
-    print(pattern)
 
     lines = []
     start = None
     gap_count = 0
+
     for i, has_text in enumerate(is_text_row):
         if has_text:
+            #If it hasn't started then it starts a new "line"
             if start is None:
                 start = i
             gap_count = 0
         else:
             if start is not None:
+                # If it started then it starts counting the line in-between text and it appends it to count it as a line
                 gap_count += 1
                 if gap_count >= min_gap:
                     lines.append((start, i - gap_count))
                     start = None
+
     if start is not None:
         lines.append((start, len(is_text_row)))
+    # Similar to a flush, incase we leave the loop and we still haven't finished the line.
+
+
+
     padding = 10
+    #Sends back the original image (not grayscale) with the text data we got from the grayscale image.
+    #Also adds 10 pixels of padding to make sure it is not cutting off the text.
     return [
         img.crop((0, max(0, top - padding), img.width, min(img.height, bottom + padding)))
         for top, bottom in lines
